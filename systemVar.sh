75df72948421ac1c11a75465ab5fea05a53eac62
aviran932 <aviran932@gmail.com>
2018-12-26 10:37:35 -0500

    echo system variables

---
+++ systemVar.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo -e "Home: $HOME\nUser: $USER\nDisplay: $DISPLAY\nTerm: $TERM\nPath: $PATH\nHistSize: $HISTSIZE\nSession_Manager: $SESSIONMANAGER\nShell $SHELL\nHostName: $HOSTNAME\nLang: $LANG\nMail: $MAIL"
